d7ca9c2edf101806a48720bc33eb391cbc17b38c
prudhvi <prudhvi@hugotechnologies.com>
2012-07-12 12:13:18 +0530

    second commit

--- src/com/hugo/main/HugoMain.java
+++ src/com/hugo/main/HugoMain.java
@@ -3,6 +3,7 @@ package com.hugo.main;
 public class HugoMain {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello good evening");
 	}
 
 }
---
+++ src/com/hugo/main/TestMain.java
@@ -0,0 +1,8 @@
+package com.hugo.main;
+
+public class TestMain {
+	public static void main(String[] args) {
+		System.out.println("good evening");
+	}
+
+}
